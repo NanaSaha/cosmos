85e0ca740c32ca44aeaa6bbb004e5b8bfcc3e63d
Nana Osafo-Bosompem <nana.osafo.bosompem@gmail.com>
2024-08-13 16:42:11 +0200

    reconfigured tests

--- public/cosmos.js
+++ public/cosmos.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const searchButton = document.getElementById('searchButton');
+    const srchbtn = document.getElementById('srchbtn');
 
-    const reloadBtn = document.getElementById('reloadBtn');
-    const destinationFilter = document.getElementById('destinationFilter');
-    const airlineFilter = document.getElementById('airlineFilter');
-    const flightTableBody = document.getElementById('flightTableBody');
+    const bkBtn = document.getElementById('bkBtn');
+    const filter_destination = document.getElementById('filter_destination');
+    const filter_airline = document.getElementById('filter_airline');
+    const getflightTableBody = document.getElementById('getflightTableBody');
 
-    const emptyNotice = document.getElementById('emptyNotice');
+    const nullinfo = document.getElementById('nullinfo');
 
-    searchButton.addEventListener('click', function () {
-        const destination = destinationFilter.value;
-        const airline = airlineFilter.value;
+    srchbtn.addEventListener('click', function () {
+        const destination = filter_destination.value;
+        const airline = filter_airline.value;
 
-        // making AJAX request to the endpoint
-        fetch('/api/flights', {
+        // making AJAX request to the endpoint and changing all inputs to uppercase
+        fetch('/cosmos/check_flights', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -26,61 +26,61 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(response => response.json())
             .then(data => {
                 // clear all data in the table
-                flightTableBody.innerHTML = '';
+                getflightTableBody.innerHTML = '';
 
                 console.log("data coming -- ", data)
 
                 if (data.length === 0) {
-                    // Show the nil notice
-                    emptyNotice.style.display = 'block';
+                    // Show the null info
+                    nullinfo.style.display = 'block';
                 } else {
-                    // Hide the empty notice if there are results
-                    emptyNotice.style.display = 'none';
+                    // Hide the null information if there are results
+                    nullinfo.style.display = 'none';
 
                     // Populate the table with the filtered data
                     data.forEach(flight => {
                         const row = document.createElement('tr');
 
-                        const flightNumberCell = document.createElement('td');
-                        flightNumberCell.textContent = flight['flight_number'];
-                        row.appendChild(flightNumberCell);
+                        const flightNumberTd = document.createElement('td');
+                        flightNumberTd.textContent = flight['flight_number'];
+                        row.appendChild(flightNumberTd);
 
-                        const airlineCell = document.createElement('td');
-                        airlineCell.textContent = flight['airline'];
-                        row.appendChild(airlineCell);
+                        const airlineTd = document.createElement('td');
+                        airlineTd.textContent = flight['airline'];
+                        row.appendChild(airlineTd);
 
-                        const originCell = document.createElement('td');
-                        originCell.textContent = flight['origin'];
-                        row.appendChild(originCell);
+                        const originTd = document.createElement('td');
+                        originTd.textContent = flight['origin'];
+                        row.appendChild(originTd);
 
-                        const destinationCell = document.createElement('td');
-                        destinationCell.textContent = flight['destination'];
-                        row.appendChild(destinationCell);
+                        const destinationTd = document.createElement('td');
+                        destinationTd.textContent = flight['destination'];
+                        row.appendChild(destinationTd);
 
-                        const scheduledDepartureCell = document.createElement('td');
-                        scheduledDepartureCell.textContent = flight['scheduled_departure_at'];
-                        row.appendChild(scheduledDepartureCell);
+                        const scheduledDepartureTd = document.createElement('td');
+                        scheduledDepartureTd.textContent = flight['scheduled_departure_at'];
+                        row.appendChild(scheduledDepartureTd);
 
-                        const actualDepartureCell = document.createElement('td');
-                        actualDepartureCell.textContent = flight['actual_departure_at'];
-                        row.appendChild(actualDepartureCell);
+                        const actualDepartureTd = document.createElement('td');
+                        actualDepartureTd.textContent = flight['actual_departure_at'];
+                        row.appendChild(actualDepartureTd);
 
-                        const delaysCell = document.createElement('td');
+                        const delaysTd = document.createElement('td');
                         const delayList = document.createElement('ul');
                         flight['delays'].forEach(delay => {
                             const delayItem = document.createElement('li');
                             delayItem.textContent = `${delay['description']} (${delay['time_minutes']} minutes)`;
                             delayList.appendChild(delayItem);
                         });
-                        delaysCell.appendChild(delayList);
-                        row.appendChild(delaysCell);
+                        delaysTd.appendChild(delayList);
+                        row.appendChild(delaysTd);
 
-                        flightTableBody.appendChild(row);
+                        getflightTableBody.appendChild(row);
                     });
 
                 }
             })
-            .catch(error => console.error('Error fetching flight data:', error));
+            .catch(error => console.error('Error fetching data:', error));
 
 
     });
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    reloadBtn.addEventListener('click', function () {
+    bkBtn.addEventListener('click', function () {
         window.location.href = '/';
     });
 
